e5cb7d22b5420d60e7bec2ac09c04b6c2030491f
Mikel Diez Garcia <mikel.diez@somorrostro.com>
2024-04-09 13:30:03 +0200

    liftoff

---
+++ 7000G_compostree/7000G_compostree.ino
@@ -0,0 +1,332 @@
+#define TINY_GSM_MODEM_SIM7000
+#define SerialMon Serial
+#define SerialAT Serial1
+#define TINY_GSM_DEBUG SerialMon
+#define TINY_GSM_USE_GPRS true
+#define TINY_GSM_USE_WIFI false
+#define GSM_PIN ""
+
+
+const char apn[]      = "nb.es.vodafone.iot";
+const char gprsUser[] = "";
+const char gprsPass[] = "";
+
+// Your WiFi connection credentials, if applicable
+const char wifiSSID[] = "YourSSID";
+const char wifiPass[] = "YourWiFiPass";
+
+// MQTT details
+const char *broker = "broker.emqx.io";
+
+const char topicLed[] = "ledTopic";
+const char topicLedStatus[] = "ledStatusTopic";
+const char topicInit[] = "initTopic";
+const char topicLat[] = "topicLat"; 
+const char topicLon[] = "topicLon";  // Topic for latitude data
+
+char lonBuffer[20]; 
+char latBuffer[20]; 
+char tmpBuffer[20]; 
+
+
+unsigned long lastLatPublish = 0;  // Timestamp of the last latitude publish
+
+float lat,  lon;
+
+#include <TinyGsmClient.h>
+#include <PubSubClient.h>
+#include <Ticker.h>
+#include <SPI.h>
+#include <SD.h>
+
+// Just in case someone defined the wrong thing..
+#if TINY_GSM_USE_GPRS && not defined TINY_GSM_MODEM_HAS_GPRS
+#undef TINY_GSM_USE_GPRS
+#undef TINY_GSM_USE_WIFI
+#define TINY_GSM_USE_GPRS false
+#define TINY_GSM_USE_WIFI true
+#endif
+#if TINY_GSM_USE_WIFI && not defined TINY_GSM_MODEM_HAS_WIFI
+#undef TINY_GSM_USE_GPRS
+#undef TINY_GSM_USE_WIFI
+#define TINY_GSM_USE_GPRS true
+#define TINY_GSM_USE_WIFI false
+#endif
+
+#ifdef DUMP_AT_COMMANDS
+#include <StreamDebugger.h>
+StreamDebugger debugger(SerialAT, SerialMon);
+TinyGsm        modem(debugger);
+#else
+TinyGsm        modem(SerialAT);
+#endif
+TinyGsmClient client(modem);
+PubSubClient  mqtt(client);
+
+
+
+Ticker tick;
+
+
+
+#define uS_TO_S_FACTOR 1000000ULL  // Conversion factor for micro seconds to seconds
+#define TIME_TO_SLEEP  60          // Time ESP32 will go to sleep (in seconds)
+
+#define UART_BAUD   115200
+#define PIN_DTR     25
+#define PIN_TX      27
+#define PIN_RX      26
+#define PWR_PIN     4
+
+#define SD_MISO     2
+#define SD_MOSI     15
+#define SD_SCLK     14
+#define SD_CS       13
+#define LED_PIN     12
+
+int ledStatus = LOW;
+
+uint32_t lastReconnectAttempt = 0;
+
+void mqttCallback(char *topic, byte *payload, unsigned int len)
+{
+
+    SerialMon.print("Message arrived [");
+    SerialMon.print(topic);
+    SerialMon.print("]: ");
+    SerialMon.write(payload, len);
+    SerialMon.println();
+
+    // Only proceed if incoming message's topic matches
+    if (String(topic) == topicLed) {
+        ledStatus = !ledStatus;
+        digitalWrite(LED_PIN, ledStatus);
+        SerialMon.print("ledStatus:");
+        SerialMon.println(ledStatus);
+        mqtt.publish(topicLedStatus, ledStatus ? "1" : "0");
+
+    }
+}
+void enableGPS(void)
+{
+    // Set Modem GPS Power Control Pin to HIGH ,turn on GPS power
+    // Only in version 20200415 is there a function to control GPS power
+    modem.sendAT("+CGPIO=0,48,1,1");
+    if (modem.waitResponse(10000L) != 1) {
+        DBG("Set GPS Power HIGH Failed");
+    }
+    modem.enableGPS();
+}
+
+void disableGPS(void)
+{
+    // Set Modem GPS Power Control Pin to LOW ,turn off GPS power
+    // Only in version 20200415 is there a function to control GPS power
+    modem.sendAT("+CGPIO=0,48,1,0");
+    if (modem.waitResponse(10000L) != 1) {
+        DBG("Set GPS Power LOW Failed");
+    }
+    modem.disableGPS();
+}
+boolean mqttConnect()
+{
+    SerialMon.print("Connecting to ");
+    SerialMon.print(broker);
+
+    // Connect to MQTT Broker
+    boolean status = mqtt.connect(broker);
+
+    // Or, if you want to authenticate MQTT:
+    // boolean status = mqtt.connect("GsmClientName", "mqtt_user", "mqtt_pass");
+
+    if (status == false) {
+        SerialMon.println(" fail");
+        return false;
+    }
+    SerialMon.println(" success");
+    mqtt.publish(topicInit, "7000G Started");
+    return mqtt.connected();
+}
+
+
+void setup()
+{
+    // Set console baud rate
+    Serial.begin(115200);
+    delay(10);
+
+    // Set LED OFF
+    pinMode(LED_PIN, OUTPUT);
+    digitalWrite(LED_PIN, HIGH);
+
+    pinMode(PWR_PIN, OUTPUT);
+    digitalWrite(PWR_PIN, HIGH);
+    // Starting the machine requires at least 1 second of low level, and with a level conversion, the levels are opposite
+    delay(1000);
+    digitalWrite(PWR_PIN, LOW);
+
+    SPI.begin(SD_SCLK, SD_MISO, SD_MOSI, SD_CS);
+    if (!SD.begin(SD_CS)) {
+        Serial.println("SDCard MOUNT FAIL");
+    } else {
+        uint32_t cardSize = SD.cardSize() / (1024 * 1024);
+        String str = "SDCard Size: " + String(cardSize) + "MB";
+        Serial.println(str);
+    }
+
+    Serial.println("\nWait...");
+
+    delay(1000);
+
+    SerialAT.begin(UART_BAUD, SERIAL_8N1, PIN_RX, PIN_TX);
+
+    // Restart takes quite some time
+    // To skip it, call init() instead of restart()
+    Serial.println("Initializing modem...");
+    if (!modem.restart()) {
+        Serial.println("Failed to restart modem, attempting to continue without restarting");
+    }
+
+
+
+    String name = modem.getModemName();
+    DBG("Modem Name:", name);
+
+    String modemInfo = modem.getModemInfo();
+    DBG("Modem Info:", modemInfo);
+
+#if TINY_GSM_USE_GPRS
+    // Unlock your SIM card with a PIN if needed
+    if (GSM_PIN && modem.getSimStatus() != 3) {
+        modem.simUnlock(GSM_PIN);
+    }
+#endif
+
+#if TINY_GSM_USE_WIFI
+    // Wifi connection parameters must be set before waiting for the network
+    SerialMon.print(F("Setting SSID/password..."));
+    if (!modem.networkConnect(wifiSSID, wifiPass)) {
+        SerialMon.println(" fail");
+        delay(10000);
+        return;
+    }
+    SerialMon.println(" success");
+#endif
+
+#if TINY_GSM_USE_GPRS && defined TINY_GSM_MODEM_XBEE
+    // The XBee must run the gprsConnect function BEFORE waiting for network!
+    modem.gprsConnect(apn, gprsUser, gprsPass);
+#endif
+
+    SerialMon.print("Waiting for network...");
+    if (!modem.waitForNetwork()) {
+        SerialMon.println(" fail");
+        delay(10000);
+        return;
+    }
+    SerialMon.println(" success");
+
+    if (modem.isNetworkConnected()) {
+        SerialMon.println("Network connected");
+    }
+
+#if TINY_GSM_USE_GPRS
+    // GPRS connection parameters are usually set after network registration
+    SerialMon.print(F("Connecting to "));
+    SerialMon.print(apn);
+    if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
+        SerialMon.println(" fail");
+        delay(10000);
+        return;
+    }
+    SerialMon.println(" success");
+
+    if (modem.isGprsConnected()) {
+        SerialMon.println("GPRS connected");
+    }
+#endif
+
+    // MQTT Broker setup
+    mqtt.setServer(broker, 1883);
+    mqtt.setCallback(mqttCallback);
+
+
+
+        enableGPS();
+
+    while (1) {
+        if (modem.getGPS(&lat, &lon)) {
+            Serial.println("The location has been locked, the latitude and longitude are:");
+            Serial.print("latitude:"); Serial.println(lat);
+            Serial.print("longitude:"); Serial.println(lon);
+            
+            dtostrf(lon, 0, 10, lonBuffer); 
+            dtostrf(lat, 0, 10, latBuffer);  // Convert the float to a char array with 6 digits after the decimal point
+            break;
+        }
+        digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+        delay(2000);
+    }
+
+    disableGPS();
+
+}
+
+void loop()
+{
+
+
+    // Make sure we're still registered on the network
+    if (!modem.isNetworkConnected()) {
+        SerialMon.println("Network disconnected");
+        if (!modem.waitForNetwork(180000L, true)) {
+            SerialMon.println(" fail");
+            delay(10000);
+            return;
+        }
+        if (modem.isNetworkConnected()) {
+            SerialMon.println("Network re-connected");
+        }
+
+#if TINY_GSM_USE_GPRS
+        // and make sure GPRS/EPS is still connected
+        if (!modem.isGprsConnected()) {
+            SerialMon.println("GPRS disconnected!");
+            SerialMon.print(F("Connecting to "));
+            SerialMon.print(apn);
+            if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
+                SerialMon.println(" fail");
+                delay(10000);
+                return;
+            }
+            if (modem.isGprsConnected()) {
+                SerialMon.println("GPRS reconnected");
+            }
+        }
+#endif
+    }
+
+    if (!mqtt.connected()) {
+        SerialMon.println("=== MQTT NOT CONNECTED ===");
+        // Reconnect every 10 seconds
+        uint32_t t = millis();
+        if (t - lastReconnectAttempt > 10000L) {
+            lastReconnectAttempt = t;
+            if (mqttConnect()) {
+                lastReconnectAttempt = 0;
+            }
+        }
+        delay(100);
+        return;
+    }
+
+    mqtt.loop();
+
+    if (millis() - lastLatPublish > 2000) {
+      lastLatPublish = millis();
+      mqtt.publish(topicLat, latBuffer);
+      mqtt.publish(topicLat, lonBuffer);
+
+    }
+
+}
